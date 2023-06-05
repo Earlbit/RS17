2cae854e060071c8fe76b0745a55e57e68008324
Earl Bit <earlbit247@gmail.com>
2023-06-05 07:51:49 +0200

    JS17

---
+++ main.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 4, 5];
+
+function getFirstElement(arr) {
+  return arr[0];
+}
+
+var firstElement = getFirstElement(numbers);
+console.log("The first element is:", firstElement);
